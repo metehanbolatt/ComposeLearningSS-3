3d7d594ed324bc58e882515337fb6034a4567317
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-10-30 22:57:04 +0300

    Google Sign Up Button

---
+++ app/src/main/java/com/metehanbolat/composelearningss3/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.metehanbolat.composelearningss3
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.metehanbolat.composelearningss3.ui.theme.ComposeLearningSS3Theme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLearningSS3Theme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    GoogleButton(
+                        text = "Sign Up with Google",
+                        loadingText = "Creating Account...",
+                        onClicked = {
+                            Log.d("googleButton","Clicked")
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeLearningSS3Theme {
+        Greeting("Android")
+    }
+}
